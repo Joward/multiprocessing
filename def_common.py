d57b728376e302787e5d05f8d621d53d29120a1a
MacXiong <mxiong@hotmail.com>
2024-10-17 18:00:48 +0800

    add file sim.py

---
+++ def_common.py
@@ -0,0 +1,6 @@
+list_run = set()
+
+def def_run(*args):
+    test = list(args)
+    list_run.add(tuple(test))
+    pass
---
+++ sim.py
@@ -0,0 +1,6 @@
+from def_common import *
+
+# def_run(cmd, option1, option2, ...)
+def_run('echo', 'A', 'aaa', 'xxx')
+def_run('echo', 'B', 'bbb')
+def_run('echo', 'C', 'ccc')
