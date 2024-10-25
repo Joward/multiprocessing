9eea80ae4174c018e8500784d0f8980ce7e1dbcd
MacXiong <mxiong@hotmail.com>
2024-10-25 23:30:32 +0800

    add func remove_duplicates

--- def_common.py
+++ def_common.py
@@ -1,6 +1,7 @@
 list_run = set()
 
-def def_run(*args):
-    test = list(args)
-    list_run.add(tuple(test))
-    pass
+def def_run(cmd_type, *args):
+    test = {}
+    test['cmd_type'] = cmd_type
+    test['cmd_opt'] = list(args)
+    list_run.append(test)
